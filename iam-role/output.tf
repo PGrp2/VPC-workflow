44acaaeb7dee9bb561cee38421a37cd28b61cba3
Sylvester-agbo <71844379+Sylvester-agbo@users.noreply.github.com>
2024-03-31 17:38:56 -0600

    Update output.tf

--- iam-role/output.tf
+++ iam-role/output.tf
@@ -1,27 +1,12 @@
-#Assume role policy for node
-data "aws_iam_policy_document" "nodes" {
-  statement {
-    sid     = "AllowEKSAssumeRole"
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
+#role for the demo
+output "node_role" {
+  value = aws_iam_role.nodes.arn
 }
 
 
-#Assume role policy for cluster
-data "aws_iam_policy_document" "eks_assume_role" {
-  statement {
-    sid     = "AllowEksAssumeRole"
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
+#role for the cluster
+output "demo_role" {
+  value = aws_iam_role.demo.arn
 }
+
 
